0f05edb9708244116b83629b34f66f2309d814cf
tn20000 <liangth.frank2015@outlook.com>
2023-02-11 23:09:01 -0800

    Test State

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <button onClick={() => setCounter(counter + 1)}>You clicked {counter} times</button>
       <StatusBar style="auto" />
     </View>
   );
