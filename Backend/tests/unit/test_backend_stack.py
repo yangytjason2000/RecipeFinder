9cabf8c680da7015935391e4faf17faa57454d35
yangytjason2000 <yy755@cornell.edu>
2023-05-20 19:48:39 -0400

    Update test_backend_stack.py

--- Backend/tests/unit/test_backend_stack.py
+++ Backend/tests/unit/test_backend_stack.py
@@ -1,7 +1,3 @@
-
-
-from backend.backend_stack import BackendStack
-
 # example tests. To run these tests, uncomment this file along with the example
 # resource in backend/backend_stack.py
 def test_python_action():
