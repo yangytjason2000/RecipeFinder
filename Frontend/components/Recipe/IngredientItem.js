a25889ce97127847ab18e9bdcb9b299847d5a767
yangytjason2000 <yy755@cornell.edu>
2023-05-31 17:39:21 -0400

    Refactor the codebase for recipe

--- Frontend/components/Recipe/IngredientItem.js
+++ Frontend/components/Recipe/IngredientItem.js
@@ -2,7 +2,7 @@ import {Text, } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { styles } from '../../styles';
 const Item = ({food,navigateToAdd}) => {
-    return(<TouchableOpacity onPress={async () => await navigateToAdd(food.name,food.quantity,food.unit,food.emoji,false)}>
+    return(<TouchableOpacity onPress={async () => await navigateToAdd(food,false)}>
       <Text style={styles.food}>{food.emoji+" "+food.name+" "+food.quantity+food.unit}</Text>
     </TouchableOpacity>)
 };
